2bda39ae8d3ccbb09280528130b040c63f832486
Nikopetaja <niko.petaja@gmail.com>
2024-02-27 13:04:52 +0200

    Second commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import Home from './pages/Home';
+import Tab1 from './pages/Tab1';
+import Tab2 from './pages/Tab2';
+import Tab3 from './pages/Tab3';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
---
+++ src/pages/Tab2.js
@@ -0,0 +1,8 @@
+// src/components/Tab1.js
+import React from 'react';
+
+const Tab2 = () => {
+  return <div>Tab 2 Content</div>;
+};
+
+export default Tab2;
