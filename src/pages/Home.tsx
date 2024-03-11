d6cd05253ecd61040e7638f3233152ea91b59163
Nikopetaja <niko.petaja@gmail.com>
2024-03-11 19:47:04 +0200

    Third commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,27 @@
 // Home.tsx
-import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonIcon, IonButtons, IonMenuButton } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import React, { useState, useEffect } from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButtons, IonMenuButton } from '@ionic/react';
+import axios from 'axios';
 import './Home.css';
 import Menu from '../components/Menu';
 
 const Home: React.FC = () => {
+  const [joke, setJoke] = useState<string>('');
+
+  useEffect(() => {
+    // Fetch Chuck Norris joke when the component mounts
+    fetchChuckNorrisJoke();
+  }, []);
+
+  const fetchChuckNorrisJoke = async () => {
+    try {
+      const response = await axios.get('https://api.chucknorris.io/jokes/random');
+      setJoke(response.data.value);
+    } catch (error) {
+      console.error('Error fetching Chuck Norris joke:', error);
+    }
+  };
+
   return (
     <>
       <Menu />
@@ -24,7 +40,9 @@ const Home: React.FC = () => {
               <IonTitle size="large">Home Page</IonTitle>
             </IonToolbar>
           </IonHeader>
-          <ExploreContainer />
+          {/* Display Chuck Norris joke */}
+          <h2>Welcome to My App</h2>
+          <p>{joke}</p>
           <div>
             <p>This is my app. <br></br> Here you can find two ways to navigate. <br></br> There is the toolbar at the bottom of the page and a side menu that opens by clicking on the button next to the header of this page.</p>
           </div>
